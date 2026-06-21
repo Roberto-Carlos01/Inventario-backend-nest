56d2f5349b1515334571fb635c27801f698a49ad
Robert <robertocarlos12345@gmail.com>
2026-06-21 14:12:34 -0400

    Modulos del sistema inventario

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { RolModule } from './modules/admin/rol/rol.module';
+import { PermissionsModule } from './modules/admin/permissions/permissions.module';
+import { InventarioModule } from './modules/admin/inventario/inventario.module';
+import { NotaModule } from './modules/admin/nota/nota.module';
+import { ClienteProveedorModule } from './modules/admin/cliente-proveedor/cliente-proveedor.module';
+import { UserModule } from './modules/admin/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.development.env', '.production.env'],
     }),
+    RolModule,
+    PermissionsModule,
+    InventarioModule,
+    NotaModule,
+    ClienteProveedorModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
