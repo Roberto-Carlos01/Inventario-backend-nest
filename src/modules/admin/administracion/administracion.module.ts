4838ccd7755628bbc1a6f0e86496e853cc0cf7db
Robert <robertocarlos12345@gmail.com>
2026-06-26 05:44:59 -0400

    administracion

--- src/modules/admin/administracion/administracion.module.ts
+++ src/modules/admin/administracion/administracion.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EmpleadoModule } from './empleado/empleado.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { RolModule } from './rol/rol.module';
+import { PermisoModule } from './permiso/permiso.module';
+import { SucursalModule } from './sucursal/sucursal.module';
 
-@Module({})
+@Module({
+  imports: [EmpleadoModule, UsuarioModule, RolModule, PermisoModule, SucursalModule]
+})
 export class AdministracionModule {}
