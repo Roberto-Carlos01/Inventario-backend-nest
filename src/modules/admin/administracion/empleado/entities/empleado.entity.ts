6727621b40ce3240fdee314f46af718c6d7a54a3
Robert <robertocarlos12345@gmail.com>
2026-06-30 10:50:27 -0400

    entity Empleado

--- src/modules/admin/administracion/empleado/entities/empleado.entity.ts
+++ src/modules/admin/administracion/empleado/entities/empleado.entity.ts
@@ -1,1 +1,31 @@
-export class Empleado {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'empleado' })
+export class Empleado {
+  @PrimaryGeneratedColumn()
+  idempleado!: number;
+
+  @Column({ type: 'varchar', length: 150, nullable: false })
+  nombreCompleto!: string;
+
+  @Column({ type: 'varchar', length: 30, unique: true })
+  ci!: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  telefono!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  direccion!: string;
+
+  @Column({ type: 'date' })
+  fecha_nac!: Date;
+
+  @Column({ type: 'date' })
+  fechaIngreso!: Date;
+
+  @Column({ type: 'decimal', precision: 12, scale: 2 })
+  salarioBase!: number;
+
+  @Column({ default: true })
+  estado!: boolean;
+}
