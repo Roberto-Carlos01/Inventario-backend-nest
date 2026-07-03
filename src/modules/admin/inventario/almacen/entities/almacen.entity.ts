e9049a14337eef8076cc4b94ed188e25c9fc652f
Robert <robertocarlos12345@gmail.com>
2026-07-03 19:09:43 -0400

    entity - dto - almacen y relacion 1:N entre almacen y sucursal

--- src/modules/admin/inventario/almacen/entities/almacen.entity.ts
+++ src/modules/admin/inventario/almacen/entities/almacen.entity.ts
@@ -1,1 +1,27 @@
-export class Almacen {}
+import { Sucursal } from 'src/modules/admin/administracion/sucursal/entities/sucursal.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'almacen' })
+export class Almacen {
+  @PrimaryGeneratedColumn()
+  idalmacen!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  nombre!: string;
+
+  @Column({ type: 'text', nullable: true })
+  descripcion?: string;
+
+  @Column({ type: 'boolean', default: true, nullable: true })
+  activo?: boolean;
+
+  @ManyToOne(() => Sucursal, (sucursal) => sucursal.almacenes)
+  @JoinColumn({ name: 'idsucursal' })
+  sucursal!: Sucursal;
+}
