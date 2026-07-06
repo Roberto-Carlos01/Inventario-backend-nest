a52170b4ab22adc37d4111db49a7b6019f97739e
Robert <robertocarlos12345@gmail.com>
2026-07-06 09:29:31 -0400

    dto inventario

--- src/modules/admin/inventario/inventario/dto/create-inventario.dto.ts
+++ src/modules/admin/inventario/inventario/dto/create-inventario.dto.ts
@@ -1,1 +1,20 @@
-export class CreateInventarioDto {}
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min } from 'class-validator';
+
+export class CreateInventarioDto {
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  idproducto!: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  idalmacen!: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  stock_minimo?: number;
+}
