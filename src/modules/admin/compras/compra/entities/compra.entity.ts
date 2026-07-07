345089db7a33926914403ae50f632e5741654442
Robert <robertocarlos12345@gmail.com>
2026-07-06 20:31:52 -0400

    entity compra

--- src/modules/admin/compras/compra/entities/compra.entity.ts
+++ src/modules/admin/compras/compra/entities/compra.entity.ts
@@ -1,1 +1,16 @@
-export class Compra {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'compra' })
+export class Compra {
+  @PrimaryGeneratedColumn()
+  idcompra!: number;
+
+  @Column({ type: 'datetime', default: 'CURRENT_TIMESTAMP' })
+  fecha!: Date;
+
+  @Column({ type: 'decimal', precision: 12, scale: 2, nullable: true })
+  total?: number;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  numero_factura_recibido?: string;
+}
