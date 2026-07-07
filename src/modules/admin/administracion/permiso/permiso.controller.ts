b371cf0342330aec55c055380b9aa8395a528b53
Robert <robertocarlos12345@gmail.com>
2026-07-07 11:00:27 -0400

    Controladores - permiso

--- src/modules/admin/administracion/permiso/permiso.controller.ts
+++ src/modules/admin/administracion/permiso/permiso.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { PermisoService } from './permiso.service';
 import { CreatePermisoDto } from './dto/create-permiso.dto';
 import { UpdatePermisoDto } from './dto/update-permiso.dto';
 
-@Controller('permiso')
+@Controller('permisos')
 export class PermisoController {
   constructor(private readonly permisoService: PermisoService) {}
 
@@ -16,19 +25,21 @@ export class PermisoController {
   findAll() {
     return this.permisoService.findAll();
   }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permisoService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.permisoService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermisoDto: UpdatePermisoDto) {
-    return this.permisoService.update(+id, updatePermisoDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updatePermisoDto: UpdatePermisoDto,
+  ) {
+    return this.permisoService.update(id, updatePermisoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permisoService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.permisoService.remove(id);
   }
 }
