e784c7240562ffef55d8a0c499aef8eb7c58aad0
Robert <robertocarlos12345@gmail.com>
2026-07-06 20:27:16 -0400

    dto proveedor

--- src/modules/admin/compras/proveedor/dto/create-proveedor.dto.ts
+++ src/modules/admin/compras/proveedor/dto/create-proveedor.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProveedorDto {}
+import { IsNotEmpty, IsString, Length, Max } from 'class-validator';
+
+export class CreateProveedorDto {
+  @IsNotEmpty()
+  @IsString()
+  @Max(150)
+  razonsocial!: string;
+
+  @IsString()
+  @Length(3, 30)
+  telefono?: string;
+
+  @IsString()
+  @Length(10, 150)
+  email?: string;
+
+  @IsString()
+  @Length(3, 50)
+  nit_ci?: string;
+
+  @IsString()
+  descripcion?: string;
+}
