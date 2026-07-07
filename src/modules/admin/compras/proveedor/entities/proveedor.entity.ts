6e06d109c95cb2c97d1234078c2326ca01154aa9
Robert <robertocarlos12345@gmail.com>
2026-07-06 20:24:21 -0400

    entity proveedor

--- src/modules/admin/compras/proveedor/entities/proveedor.entity.ts
+++ src/modules/admin/compras/proveedor/entities/proveedor.entity.ts
@@ -1,1 +1,22 @@
-export class Proveedor {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'proveedor' })
+export class Proveedor {
+  @PrimaryGeneratedColumn()
+  idproveedor!: number;
+
+  @Column({ type: 'varchar', length: 150 })
+  razonsocial!: string;
+
+  @Column({ type: 'varchar', length: 30, nullable: true })
+  telefono?: string;
+
+  @Column({ type: 'varchar', length: 150, nullable: true })
+  email?: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  nit_ci?: string;
+
+  @Column({ type: 'text', nullable: true })
+  descripcion?: string;
+}
